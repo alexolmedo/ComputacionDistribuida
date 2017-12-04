4a8327c92788b4b9d82eb86a2ccd10c95e1c01ac
alexander <olmedo2408@gmail.com>
2017-12-04 14:39:45 -0500

    Agregado arreglo de threads

--- 2017.11.30.con_server.py
+++ 2017.11.30.con_server.py
@@ -3,7 +3,7 @@ import sys
 from thread import *
  
 HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
+PORT = 12222 # Arbitrary non-privileged port
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -41,12 +41,13 @@ def clientthread(conn):
     conn.close()
  
 #now keep talking with the client
+threads = []
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
      
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
+    threads.append(start_new_thread(clientthread ,(conn,)))
  
 s.close()
