d3c4be373325878ba9d3f38e129c75bd8bc95eec
alexander <olmedo2408@gmail.com>
2018-01-08 15:45:30 -0500

    Nuevo demonio, ahora funcionando

--- 2018.01.08.daemon.c
+++ 2018.01.08.daemon.c
@@ -1,89 +1,175 @@
-/*
- * daemon.c
- * This example daemonizes a process, writes a few log messages,
- * sleeps 20 seconds and terminates afterwards.
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <syslog.h>
-
-static void skeleton_daemon()
-{
-    pid_t pid;
-
-    /* Fork off the parent process */
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
-
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-
-    /* Fork off for the second time*/
-    pid = fork();
-
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    /* Set new file permissions */
-    umask(0);
-
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
-
-    /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>0; x--)
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+ 
+#define DAEMON_NAME "simpledaemon"
+ 
+ 
+    void daemonShutdown();
+    void signal_handler(int sig);
+    void daemonize(char *rundir, char *pidfile);
+ 
+ 
+    int pidFilehandle;
+ 
+ 
+    void signal_handler(int sig)
     {
-        close (x);
+        switch(sig)
+        {
+            case SIGHUP:
+                syslog(LOG_WARNING, "Received SIGHUP signal.");
+                break;
+            case SIGINT:
+            case SIGTERM:
+                syslog(LOG_INFO, "Daemon exiting");
+                daemonShutdown();
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                syslog(LOG_WARNING, "Unhandled signal %s", strsignal(sig));
+                break;
+        }
     }
-
-    /* Open the log file */
-    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
-}
-
-int main()
-{
-    skeleton_daemon();
-
-    FILE * fp;
-    fp = fopen ("log.txt", "a");
-    fprintf(fp, "Daemon log message\n");
-    fclose(fp);
-
-    while (1)
+     
+    void daemonShutdown()
     {
-        //TODO: Insert daemon code here.
-        syslog (LOG_NOTICE, "Custom daemon started.");
-        sleep (20);
-        break;
+        close(pidFilehandle);
     }
-
-    syslog (LOG_NOTICE, "Custom daemon terminated.");
-    closelog();
-
-    return EXIT_SUCCESS;
-}
+ 
+    void daemonize(char *rundir, char *pidfile)
+    {
+        int pid, sid, i;
+        char str[10];
+        struct sigaction newSigAction;
+        sigset_t newSigSet;
+         
+        /* Check if parent process id is set */
+        if (getppid() == 1)
+        {
+            /* PPID exists, therefore we are already a daemon */
+            return;
+        }
+ 
+        /* Set signal mask - signals we want to block */
+        sigemptyset(&newSigSet);
+        sigaddset(&newSigSet, SIGCHLD);  /* ignore child - i.e. we don't need to wait for it */
+        sigaddset(&newSigSet, SIGTSTP);  /* ignore Tty stop signals */
+        sigaddset(&newSigSet, SIGTTOU);  /* ignore Tty background writes */
+        sigaddset(&newSigSet, SIGTTIN);  /* ignore Tty background reads */
+        sigprocmask(SIG_BLOCK, &newSigSet, NULL);   /* Block the above specified signals */
+         
+        /* Set up a signal handler */
+        newSigAction.sa_handler = signal_handler;
+        sigemptyset(&newSigAction.sa_mask);
+        newSigAction.sa_flags = 0;
+         
+            /* Signals to handle */
+            sigaction(SIGHUP, &newSigAction, NULL);     /* catch hangup signal */
+            sigaction(SIGTERM, &newSigAction, NULL);    /* catch term signal */
+            sigaction(SIGINT, &newSigAction, NULL);     /* catch interrupt signal */
+ 
+         
+        /* Fork*/
+        pid = fork();
+         
+        if (pid < 0)
+        {
+            /* Could not fork */
+            exit(EXIT_FAILURE);
+        }
+         
+        if (pid > 0)
+        {
+            /* Child created ok, so exit parent process */
+            printf("Child process created: %d\n", pid);
+            exit(EXIT_SUCCESS);
+        }
+         
+        /* Child continues */
+         
+        umask(027); /* Set file permissions 750 */
+         
+        /* Get a new process group */
+        sid = setsid();
+         
+        if (sid < 0)
+        {
+            exit(EXIT_FAILURE);
+        }
+         
+        /* close all descriptors */
+        for (i = getdtablesize(); i >= 0; --i)
+        {
+            close(i);
+        }
+         
+        /* Route I/O connections */
+ 
+        /* Open STDIN */
+        i = open("/dev/null", O_RDWR);
+ 
+        /* STDOUT */
+        dup(i);
+ 
+        /* STDERR */
+        dup(i);
+         
+        chdir(rundir); /* change running directory */
+         
+        /* Ensure only one copy */
+        pidFilehandle = open(pidfile, O_RDWR|O_CREAT, 0600);
+         
+        if (pidFilehandle == -1 )
+        {
+            /* Couldn't open lock file */
+            syslog(LOG_INFO, "Could not open PID lock file %s, exiting", pidfile);
+            exit(EXIT_FAILURE);
+        }
+         
+        /* Try to lock file */
+        if (lockf(pidFilehandle,F_TLOCK,0) == -1)
+        {
+            /* Couldn't get lock on lock file */
+            syslog(LOG_INFO, "Could not lock PID lock file %s, exiting", pidfile);
+            exit(EXIT_FAILURE);
+        }
+ 
+         
+        /* Get and format PID */
+        sprintf(str,"%d\n",getpid());
+         
+        /* write pid to lockfile */
+        write(pidFilehandle, str, strlen(str));
+    }
+     
+    int main()
+    {
+        /* Debug logging
+        setlogmask(LOG_UPTO(LOG_DEBUG));
+        openlog(DAEMON_NAME, LOG_CONS, LOG_USER);
+        */
+         
+        /* Logging */
+        setlogmask(LOG_UPTO(LOG_INFO));
+        openlog(DAEMON_NAME, LOG_CONS | LOG_PERROR, LOG_USER);
+         
+        syslog(LOG_INFO, "Daemon starting up");
+ 
+        /* Deamonize */
+        daemonize("/tmp/", "/tmp/daemon.pid");
+         
+        syslog(LOG_INFO, "Daemon running");
+         
+        while (1)
+        {
+            syslog(LOG_INFO, "daemon says hello");
+             
+            sleep(1);
+        }
+    }
